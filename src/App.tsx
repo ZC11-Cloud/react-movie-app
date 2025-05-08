5067947048b768337481f2884b300e00ff4476f7
zhangchi2206040227 <2942435009@xiaomi.com>
2025-05-08 19:39:58 +0800

    feat: 完成header

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Search from "./components/Search.tsx";
+import { useState } from "react";
+const App = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    return (
+        <main>
+            <div className="pattern"></div>
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero Banner" />
+                    <h1>
+                        Find <span className="text-gradient">Movies</span>{" "}
+                        You'll Enjoy Without the Hassle
+                    </h1>
+                    <Search
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                    ></Search>
+                </header>
+            </div>
+        </main>
+    );
+};
+
+export default App;
